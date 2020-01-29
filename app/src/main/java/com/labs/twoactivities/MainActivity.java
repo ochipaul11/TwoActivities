6c6cc34a8747f43f0ad49e38411c342461df4828
PAUL OKEYO <paulokeyo@hotmail.com>
2020-01-29 22:52:28 +0300

    Initial commit

---
+++ app/src/main/java/com/labs/twoactivities/MainActivity.java
@@ -0,0 +1,25 @@
+package com.labs.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private	static	final	String	LOG_TAG	= MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG,	"Button	clicked!");
+        Intent intent	=	new	Intent(this, activity_second.class);
+        startActivity(intent);
+    }
+}
