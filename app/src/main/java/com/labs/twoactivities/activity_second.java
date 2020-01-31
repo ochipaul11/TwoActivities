6559ab5fb0a25126adba59e9be22960471795614
PAUL OKEYO <paulokeyo@hotmail.com>
2020-01-31 11:41:39 +0300

    complete

--- app/src/main/java/com/labs/twoactivities/activity_second.java
+++ app/src/main/java/com/labs/twoactivities/activity_second.java
@@ -1,14 +1,38 @@
 package com.labs.twoactivities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class activity_second extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.labs.android.twoactivities.extra.REPLY";
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = (TextView) findViewById(R.id.text_message);
+
+        textView.setText(message);
+
+        mReply = (EditText) findViewById(R.id.editText_second);
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+
     }
 }
